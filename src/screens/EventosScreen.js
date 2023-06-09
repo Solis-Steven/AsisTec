6cc8e47b85be3826460c172c19a2b6f23e056b33
AndrikDJS <andriksj2208@gmail.com>
2023-06-09 15:44:28 -0600

    Fixed onDelete eventos

--- src/screens/EventosScreen.js
+++ src/screens/EventosScreen.js
@@ -83,6 +83,7 @@ const EventosScreen = () => {
 
     if(newItemsArray.length === 0) {
       delete eventItems[item["date"]];
+      AsyncStorage.setItem("storedEvents", JSON.stringify(eventItems));
 
       if(Object.keys(eventItems).length === 0) {
         setEventItems({"init": "init"});
