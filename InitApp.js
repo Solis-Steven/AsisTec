e15ccce415b11ebd7e5b9983150bce5e64c00911
AndrikDJS <andriksj2208@gmail.com>
2023-06-11 23:43:25 -0600

    warning fixed

--- InitApp.js
+++ InitApp.js
@@ -1,60 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from 'expo-splash-screen';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import useData from "./src/hooks/useData";
 
 const InitApp = () => {
-
-  //estado para saber si la app esta lista con los datos del usuario
   const [isReady, setIsReady] = useState(false);
-  //estado para guardar los datos del usuario
   const { setEventItems } = useData();
-
   const { setListaComponents } = useData([]);
 
-
-  //funcion para cargar los datos del usuario desde el AsyncStorage
-  const LoadData = async () => {
-    //AsyncStorage.clear();
-    await AsyncStorage.getItem("storedEvents").then(value => {
-
+  const loadData = async () => {
+    await AsyncStorage.getItem("storedEvents").then((value) => {
       if (value !== null) {
-
         setEventItems(JSON.parse(value));
       }
     }).catch((error) => {
       console.log(error);
-    }
-    );
+    });
 
-    await AsyncStorage.getItem("listaComponents").then(value => {
-
+    await AsyncStorage.getItem("listaComponents").then((value) => {
       if (value !== null) {
         setListaComponents(JSON.parse(value));
       }
     }).catch((error) => {
       console.log(error);
-    }
-    );
-  }
+    });
+  };
 
-  // Se ejecuta la funcion LoadData una vez que la app se inicia
   useEffect(() => {
-    LoadData().then(() => {
-      setIsReady(true);
+    SplashScreen.preventAutoHideAsync().then(() => {
+      loadData().then(() => {
+        setIsReady(true);
+        SplashScreen.hideAsync();
+      });
     });
   }, []);
 
   if (!isReady) {
-    return <AppLoading />;
+    return null; // Renderiza un componente de carga personalizado si es necesario
   }
 
-
-
   return (
-
     <NavigationContainer>
       <StackNavigator />
     </NavigationContainer>
